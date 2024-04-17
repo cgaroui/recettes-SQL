dcf7998a747ea95d37e79fcd583e0eda9d596f0c
chaima garoui <chaimagaroui2@gmail.com>
2024-04-17 17:00:21 +0200

    jusqua exo 16

--- requettes.sql
+++ requettes.sql
@@ -42,17 +42,73 @@
 
 
 --7- Supprimer la recette n°2 de la base de données
---8- Afficher le prix total de la recette n°5
-9- Afficher le détail de la recette n°5 (liste des ingrédients, quantités et prix)
-10- Ajouter un ingrédient en base de données : Poivre, unité : cuillère à café, prix : 2.5 €
-11- Modifier le prix de l’ingrédient n°12 (prix à votre convenance)
-12- Afficher le nombre de recettes par catégories : X entrées, Y plats, Z desserts
-13- Afficher les recettes qui contiennent l’ingrédient « Poulet »
-14- Mettez à jour toutes les recettes en diminuant leur temps de préparation de 5 minutes 
-15- Afficher les recettes qui ne nécessitent pas d’ingrédients coûtant plus de 2€ par unité de mesure
-16- Afficher la / les recette(s) les plus rapides à préparer
-17- Trouver les recettes qui ne nécessitent aucun ingrédient (par exemple la recette de la tasse d’eau 
-chaude qui consiste à verser de l’eau chaude dans une tasse)
+    DELETE FROM recette
+    WHERE id_recette =2;
+
+
+--8- Afficher le prix total de la recette n°7
+    SELECT SUM(prix*quantite) AS prixRecette
+    FROM ingredient
+    INNER JOIN contenir ON ingredient.id_ingredient = contenir.id_ingredient
+    WHERE id_recette = 7;
+
+
+
+--9- Afficher le détail de la recette n°8 (liste des ingrédients, quantités et prix)
+    SELECT nom, quantite,(prix*quantite)
+    FROM ingredient
+    INNER JOIN contenir ON ingredient.id_ingredient = contenir.id_ingredient
+    WHERE id_recette =7;
+
+
+
+
+--10- Ajouter un ingrédient en base de données : Poivre, unité : cuillère à café, prix : 2.5 €
+    INSERT INTO ingredient (nom, uniteDeMesure, prix)
+    VALUES ('poivre', 'cuillère à café', 2.50);
+
+
+--11- Modifier le prix de l’ingrédient n°12 (prix à votre convenance)
+    UPDATE ingredient 
+    SET prix = 1.50
+    WHERE id_ingredient= 12;
+
+--12- Afficher le nombre de recettes par catégories : X entrées, Y plats, Z desserts
+
+    SELECT nom_categorie, COUNT(id_recette)
+    FROM recette
+    INNER JOIN categorie ON recette.id_categorie = categorie.id_categorie
+    GROUP BY categorie.id_categorie ;
+
+
+
+--13- Afficher les recettes qui contiennent l’ingrédient « Poulet »
+    SELECT recette.nom 
+    FROM recette
+    INNER JOIN contenir ON recette.id_recette = contenir.id_recette
+    INNER JOIN ingredient ON contenir.id_ingredient = ingredient.id_ingredient
+    WHERE ingredient.nom LIKE '%poulet%';
+
+
+--14- Mettez à jour toutes les recettes en diminuant leur temps de préparation de 5 minutes 
+    UPDATE recette
+    SET duree = (duree - 5)
+    WHERE recette.duree > 5;
+
+
+--15- Afficher les recettes qui ne nécessitent pas d’ingrédients coûtant plus de 2€ par unité de mesure
+    SELECT distinct recette.nom
+    FROM recette 
+    INNER JOIN contenir ON recette.id_recette = contenir.id_recette
+    INNER JOIN ingredient ON contenir.id_ingredient = ingredient.id_ingredient
+    WHERE ingredient.prix <= 2;
+
+
+
+--16- Afficher la / les recette(s) les plus rapides à préparer
+
+
+17- Trouver les recettes qui ne nécessitent aucun ingrédient (par exemple la recette de la tasse d’eau chaude qui consiste à verser de l’eau chaude dans une tasse)
 18- Trouver les ingrédients qui sont utilisés dans au moins 3 recettes
 19- Ajouter un nouvel ingrédient à une recette spécifique
 20- Bonus : Trouver la recette la plus coûteuse de la base de données (il peut y avoir des ex aequo, il est 
